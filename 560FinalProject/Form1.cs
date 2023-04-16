ce1b5b9510138ed5803cb899f5be4fcccb27ed93
Colin O'Donnell <codonnell@ksu.edu>
2023-04-15 20:43:44 -0500

    Initial Push

--- 560FinalProject/Form1.cs
+++ 560FinalProject/Form1.cs
@@ -16,5 +16,10 @@ namespace _560FinalProject
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
