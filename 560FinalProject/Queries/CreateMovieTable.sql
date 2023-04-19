3cb6bd1c3a7bb961af57e7461fc1d3aff7fb79e7
Ryan Rosenkoetter <rjrosenkoetter@ksu.edu>
2023-04-18 21:26:02 -0500

    Added Queries Folder + base way to add the table to your local DB (make sure this is done right!)

---
+++ 560FinalProject/Queries/CreateMovieTable.sql
@@ -0,0 +1,16 @@
+﻿IF OBJECT_ID(N'MovieOperations.Movie') IS NULL
+BEGIN
+   CREATE TABLE MovieOperations.Movie
+   (
+      MovieId INT NOT NULL IDENTITY(1, 1),
+      Title NVARCHAR(128) NOT NULL,
+      Duration INT NOT NULL,
+      ReleaseYear INT NOT NULL,
+      Revenue DECIMAL(5, 2) NOT NULL,
+
+      CONSTRAINT [PK_MovieOperations_Movie_MovieId] PRIMARY KEY CLUSTERED
+      (
+         MovieId ASC
+      )
+   );
+END;
---
+++ 560FinalProject/Queries/MovieOperationSchema.sql
@@ -0,0 +1,9 @@
+﻿IF NOT EXISTS
+   (
+      SELECT *
+      FROM sys.schemas s
+      WHERE s.[name] = N'MovieOperations'
+   )
+BEGIN
+   EXEC(N'CREATE SCHEMA [MovieOperations] AUTHORIZATION [dbo]');
+END;
