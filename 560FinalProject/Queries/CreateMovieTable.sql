2cc649890bae96bac25c3f4ba9b565e95ae1d84a
Colin O'Donnell <codonnell@ksu.edu>
2023-04-19 00:20:27 -0500

    created my local database and made table and inserted values to said table

--- 560FinalProject/Queries/CreateMovieTable.sql
+++ 560FinalProject/Queries/CreateMovieTable.sql
@@ -14,3 +14,54 @@ BEGIN
       )
    );
 END;
+
+GO
+
+IF SCHEMA_ID(N'MovieOpperations') IS NULL
+	EXEC(N'CREATE SCHEMA [MovieOpperations];');
+GO
+
+    DROP TABLE IF EXISTS MovieOpperations.Movie
+
+   CREATE TABLE MovieOpperations.Movie
+   (
+      MovieId INT NOT NULL IDENTITY(1, 1),
+      Title NVARCHAR(128) NOT NULL,
+      ReleaseYear INT NOT NULL,
+	  Duration INT NOT NULL,
+      Revenue NVARCHAR(128) NULL
+
+      CONSTRAINT [PK_MovieOperations_Movie_MovieId] PRIMARY KEY CLUSTERED
+      (
+         MovieId ASC
+      )
+   );
+
+	CREATE TABLE MovieOpperations.MovieCast
+	(
+		CastID INT NOT NULL IDENTITY(1,1),
+		MovieID INT NOT NULL,
+		ActorID INT NOT NULL,
+
+		CONSTRAINT [PK_MovieOperations_MovieCast_CastId] PRIMARY KEY CLUSTERED
+      (
+         CastID ASC
+      )
+	);
+
+	BULK
+    INSERT MovieOpperations.Movie
+    FROM 'E:\CIS 560\560FinalProject\movies.csv'
+    WITH
+    (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n'
+    )
+    GO
+    SELECT *
+    FROM MovieOpperations.Movie
+
+    BULK
+    INSERT MovieOpperations.MovieCast
+    FROM 'E:\CIS 560\560FinalProject\'
