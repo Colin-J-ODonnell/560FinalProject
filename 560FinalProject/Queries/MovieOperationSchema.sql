8ba81e128602c00f0421f4e32151fad6f9c54a57
Sebastian Trill <strill@ksu.edu>
2023-04-20 20:22:10 -0500

    Removed some unneeded sql files from the queries folder

--- 560FinalProject/Queries/MovieOperationSchema.sql
+++
@@ -1,9 +0,0 @@
-﻿IF NOT EXISTS
-   (
-      SELECT *
-      FROM sys.schemas s
-      WHERE s.[name] = N'MovieOperations'
-   )
-BEGIN
-   EXEC(N'CREATE SCHEMA [MovieOperations] AUTHORIZATION [dbo]');
-END;
