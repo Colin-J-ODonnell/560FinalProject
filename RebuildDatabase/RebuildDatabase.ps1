babf9808210acfe4cfda3d5f31e752d132e1c3a5
Sebastian Trill <strill@ksu.edu>
2023-04-23 18:06:08 -0500

    Starting to make the database within the project.

---
+++ RebuildDatabase/RebuildDatabase.ps1
@@ -0,0 +1,59 @@
+﻿#
+# Script.ps1
+#
+
+Param(
+   [string] $Server = "(localdb)\MSSQLLocalDb",
+   [string] $Database = "DatabaseProject"
+)
+
+# This script requires the SQL Server module for PowerShell.
+# The below commands may be required.
+
+# To check whether the module is installed.
+# Get-Module -ListAvailable -Name SqlServer;
+
+# Install the SQL Server Module
+# Install-Module -Name SqlServer -Scope CurrentUser
+
+$CurrentDrive = (Get-Location).Drive.Name + ":"
+
+Write-Host ""
+Write-Host "Rebuilding database $Database on $Server..."
+
+<#
+   If on your local machine, you can drop and re-create the database.
+#>
+& ".\Scripts\DropDatabase.ps1" -Database $Database
+& ".\Scripts\CreateDatabase.ps1" -Database $Database
+
+<#
+   If on the department's server, you don't have permissions to drop or create databases.
+   In this case, maintain a script to drop all tables.
+#>
+Write-Host "Dropping tables..."
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Tables\DropTables.sql"
+
+Write-Host "Creating schema..."
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Schemas\MovieOperations.sql"
+
+Write-Host "Creating tables..."
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Tables\CreateTables.sql"
+
+Write-Host "Stored procedures..."
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Procedures\MovieOperations.CreateActor.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Procedures\MovieOperations.CreateMovieShowtime.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Procedures\MovieOperations.CreateRoom.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Procedures\MovieOperations.CreateTheater.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Procedures\MovieOperations.RemoveActor.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Procedures\MovieOperations.RemoveMovieShowtime.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Procedures\MovieOperations.RemoveRoom.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Procedures\MovieOperations.RemoveTheater.sql"
+
+Write-Host "Inserting data..."
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Data\PopulateTables.sql"
+
+Write-Host "Rebuild completed."
+Write-Host ""
+
+Set-Location $CurrentDrive
