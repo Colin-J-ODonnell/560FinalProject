7d547bc7cee3e432bdb5b2aca7a6c1c20f952a81
Ryan Rosenkoetter <rjrosenkoetter@ksu.edu>
2023-05-03 12:04:41 -0500

    never done .-.

--- 560FinalProject/SQL/Procedures/MovieOperations.RemoveMovie.sql
+++ 560FinalProject/SQL/Procedures/MovieOperations.RemoveMovie.sql
@@ -1,6 +1,11 @@
 ﻿CREATE OR ALTER PROCEDURE MovieOperations.RemoveMovie
     @MovieID INT
 AS
-
+DELETE MC FROM MovieOperations.MovieCast MC INNER JOIN MovieOperations.Movie M ON M.MovieID = MC.MovieID
+WHERE M.MovieID = @MovieID;
+DELETE ST FROM MovieOperations.MovieShowtime ST INNER JOIN MovieOperations.Movie M ON M.MovieID = ST.MovieID
+WHERE M.MovieID = @MovieID;
+DELETE MG FROM MovieOperations.MovieGenres MG INNER JOIN MovieOperations.Movie M ON M.MovieID = MG.MovieID
+WHERE MG.MovieID = @MovieID;
 DELETE FROM MovieOperations.Movie
 WHERE MovieID = @MovieID
